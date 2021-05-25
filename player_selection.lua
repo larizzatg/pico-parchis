08f5e92aba4d9d4f918d8867e4dff1249ef6043f
larizza <larizza@mctekk.com>
2021-05-25 00:03:09 -0400

    fix: player selection

--- player_selection.lua
+++ player_selection.lua
@@ -1,7 +1,8 @@
 player_selection={
   selected_player=1,
   selected_house=1,
-  finished=false
+  finished=false,
+  house_pos=1
 }
 
 function player_selection.update(self)
@@ -11,19 +12,29 @@ function player_selection.update(self)
 end
 
 function player_selection.choose_house(self)
-  if btnp(1) and self.selected_house < 4 then 
-    self.selected_house += 1
+  local available_houses={}
+  for i=1, #houses do 
+    if houses[i].p == 0 then
+      add(available_houses, i)
+    end
   end
 
-  if btnp(0) and self.selected_house > 1 then 
-    self.selected_house -= 1
+  if btnp(1) and self.house_pos < #available_houses then 
+    self.house_pos += 1
   end
+
+  if btnp(0) and self.house_pos > 1 then 
+    self.house_pos -= 1
+  end
+  self.selected_house = available_houses[self.house_pos]
 
   if btnp(5) then
     players[self.selected_player].house = self.selected_house
+    houses[self.selected_house].p = self.selected_player
     if self.selected_player < #players then      
       self.selected_player+=1
-      self.selected_house=1
+      self.selected_house=0
+      self.house_pos = 1
     else
       self.finished = true
     end
