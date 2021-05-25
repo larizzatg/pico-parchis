6ba1167c50e645e696fb1aa3c761710463ddc7bc
larizza <larizza@mctekk.com>
2021-05-24 23:46:40 -0400

    feat: finish selection if no more players to set

--- player_selection.lua
+++ player_selection.lua
@@ -1,12 +1,13 @@
 player_selection={
   selected_player=1,
   selected_house=1,
-  available_houses={}
+  finished=false
 }
 
 function player_selection.update(self)
-  self:update_available_houses()
-  self:choose_house()
+  if not self.finished then 
+    self:choose_house()
+  end 
 end
 
 function player_selection.choose_house(self)
@@ -20,20 +21,13 @@ function player_selection.choose_house(self)
 
   if btnp(5) then
     players[self.selected_player].house = self.selected_house
- 
-    self.selected_player+=1
-    self.selected_house=1
-  end
-end
-
-function player_selection.update_available_houses(self)
-  local available_houses={1, 2, 3, 4}
-  for i=1, #players do 
-    if players[i].house > 0 then
-      del(available_houses, players[i].house)
+    if self.selected_player < #players then      
+      self.selected_player+=1
+      self.selected_house=1
+    else
+      self.finished = true
     end
   end
-  self.available_houses = available_houses
 end
 
 function player_selection.draw(self)
@@ -43,9 +37,11 @@ function player_selection.draw(self)
   line(40, 25, 85, 25, 9)
   line(40, 26, 85, 26, 8)
 
-
-  print("choose house (❎)", 32, 35, 5)
-
+  if self.finished then 
+    print("start game (❎)", 34, 35, 5)
+  else 
+    print("choose house (❎)", 32, 35, 5)
+  end
   
   -- print houses
   local house_size=6
@@ -65,20 +61,4 @@ function player_selection.draw(self)
       print(players[i].name, house_ix+(house_m*players[i].house), house_y-8, houses[players[i].house].c)
     end
   end
-
-  
-  -- print("p1", house_ix+(house_m*1), house_y-8, 5)
-  -- print players
-  -- local x=48
-  -- for i=1, #players do
-  --   local y = 20+(i*15)
-  --   print(players[i].name, x, y, 5)
-  --   print("<", x+16, y, 5)
-  --   rect(x+20, y, x+24, y+4, 5)
-  --   print(">", x+26, y, 5)
-
-  --   if i == self.selected_player then
-  --     line(x-1, y+6, x+7, y+6, 10)
-  --   end
-  -- end
 end
